fc576e6319156d507e12c5d34bf0745fe0f45bfe
test <83575961+rueiwoqpqpwoeiru@users.noreply.github.com>
2022-08-15 09:54:30 +0900

    Update main.cu

--- main.cu
+++ main.cu
@@ -6,9 +6,9 @@
 
 int main(int argc, char* argv[]) {
     argv[1] = "test_img.tif";
-	argv[2] = ".";
-        unit_test_Basic(argc, argv);
-	unit_test_Filter(argc, argv);
-	unit_test_Warp(argc, argv);
-	return 0;
+    argv[2] = ".";
+    unit_test_Basic(argc, argv);
+    unit_test_Filter(argc, argv);
+    unit_test_Warp(argc, argv);
+    return 0;
 };
